a5223dac35ea498f8bd4e8525619eb9f2b42315a
Amogelang <amogelangthompson@gmail.com>
2025-08-03 12:07:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/:date?", (req, res) => {
+app.get("/api/", (req, res) => {
+  const now = new Date(); // Always fresh!
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString(),
+  });
+});
+
+app.get("/api/:date", (req, res) => {
   let dateInput = req.params.date;
 
   let date;
